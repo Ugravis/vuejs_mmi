1a35e8aef92885112f8d16ab5fb3ba1ed270392f
Ulysse Pennetier <upennetier@gmail.com>
2026-02-04 16:10:02 +0100

    feat(exo2): A

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
 </script>
 
 <template>
-  <h1>Hello world TD</h1>
+  <h1>Hello world TD - Exo2 </h1>
 
-  <button @click="isVisible = !isVisible">Rendre visible</button>
+  <button @click="isVisible = !isVisible">Texte caché</button>
 
   <p v-if="isVisible">Je suis le texte caché</p>
+  <p v-else>Je suis le texte de base</p>
 </template>
 
 <style scoped>
