c36970aa92dda07e8c24f2a9de51decdb5e929a4
Ulysse Pennetier <upennetier@gmail.com>
2026-03-11 14:22:51 +0100

    * -- New part: with api platform - init -- *

---
+++ with_api_platform/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  const username = 'Ulysse'
+</script>
+
+<template>
+  <h1>MMI World</h1>
+
+  <router-link :to="{name: 'home'}">Home</router-link>
+  <router-link :to="{name: 'about'}">About</router-link>
+  <router-link :to="{name: 'user', params: {username: username}}">User</router-link>
+  <router-link :to="{name: 'countries'}">Countries</router-link>
+
+  <hr>
+
+  <router-view></router-view>
+</template>
+
+<style scoped>
+  a {
+    margin-right: 10px;
+  }
+</style>
